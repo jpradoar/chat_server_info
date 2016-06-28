0b8504d03bb76d746d03d63c71a618c149039e03
jpradoar <jpradoar@gmail.com>
2016-06-27 23:47:24 -0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -16,6 +16,7 @@ comando = comando[0:len(comando)-1]
 if comando == "help":
         print "Comandos \n help\n voz\n txt\n  espacio\n uptime\n date\n w\n load\n mem\n lan\n wlan\n cal\n "
 
+
 if '.txt' in comando:
     	texto = comando[0:len(comando)-4]
 	print texto
@@ -23,6 +24,24 @@ if '.txt' in comando:
 if '.voz' in comando:
     	texto = comando[0:len(comando)-4]
 	os.system("echo " + texto + " | festival --tts --language spanish")
+
+if '.box' in comando:
+        texto = comando[0:len(comando)-4]
+	texto = "'" + texto + "'"
+	os.system("zenity --info --text=" + texto + "")
+
+if '.msg' in comando:
+        texto = comando[0:len(comando)-4]
+        texto = "'" + texto + "'"
+        os.system("notify-send " + texto + "")
+        
+if '.tty' in comando:
+        texto = comando[0:len(comando)-4]
+        texto = "'" + texto + "'"
+        os.system("echo " + texto + " | wall")
+
+
+
 
 if comando == "hola":
 	print "Hola, soy un Linux.\n Escribe help para ver los comandos disponibles"
